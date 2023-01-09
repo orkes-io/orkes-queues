400575df0323607881a92fe4a0342b388df1572f
Viren Baraiya <virenx@gmail.com>
2023-01-09 10:04:25 -0800

    fix the issue with the queues freezing

--- orkes-redis-queues/src/main/java/io/orkes/conductor/mq/redis/QueueMonitor.java
+++ orkes-redis-queues/src/main/java/io/orkes/conductor/mq/redis/QueueMonitor.java
@@ -1,8 +1,16 @@
+/*
+ * Copyright 2022 Orkes, Inc.
+ * <p>
+ * Licensed under the Orkes Community License (the "License"); you may not use this file except in compliance with
+ * the License. You may obtain a copy of the License at
+ * <p>
+ * https://github.com/orkes-io/licenses/blob/main/community/LICENSE.txt
+ * <p>
+ * Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on
+ * an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations under the License.
+ */
 package io.orkes.conductor.mq.redis;
-
-import com.google.common.util.concurrent.Uninterruptibles;
-import io.orkes.conductor.mq.QueueMessage;
-import lombok.extern.slf4j.Slf4j;
 
 import java.math.BigDecimal;
 import java.time.Clock;
@@ -11,6 +19,11 @@ import java.util.List;
 import java.util.concurrent.*;
 import java.util.concurrent.atomic.AtomicInteger;
 
+import io.orkes.conductor.mq.QueueMessage;
+
+import com.google.common.util.concurrent.Uninterruptibles;
+import lombok.extern.slf4j.Slf4j;
+
 @Slf4j
 public abstract class QueueMonitor {
 
@@ -18,37 +31,35 @@ public abstract class QueueMonitor {
 
     private final Clock clock;
 
-    private final LinkedBlockingQueue<QueueMessage> peekedMessages;
+    private final LinkedBlockingDeque<QueueMessage> peekedMessages;
 
     private final ExecutorService executorService;
 
-    private final AtomicInteger pollCount = new AtomicInteger(10_000);
+    private final AtomicInteger pollCount = new AtomicInteger(0);
     private final String queueName;
 
     private int queueUnackTime = 30_000;
 
-    private long nextUpdate = 0;
-
     private long size = 0;
 
-    private int maxPollCount = 200;
+    private int maxPollCount = 100;
 
     public QueueMonitor(String queueName) {
         this.queueName = queueName;
         this.clock = Clock.systemDefaultZone();
-        this.peekedMessages = new LinkedBlockingQueue<>();
-        this.executorService = new ThreadPoolExecutor(1, 1,
-                100L, TimeUnit.MILLISECONDS,
-                new ArrayBlockingQueue<>(maxPollCount));
+        this.peekedMessages = new LinkedBlockingDeque<>();
+        this.executorService =
+                new ThreadPoolExecutor(
+                        1, 1, 0L, TimeUnit.MILLISECONDS, new ArrayBlockingQueue<>(maxPollCount));
     }
 
     public List<QueueMessage> pop(int count, int waitTime, TimeUnit timeUnit) {
 
         List<QueueMessage> messages = new ArrayList<>();
-        pollCount.addAndGet(count);
+        int pendingCount = pollCount.addAndGet(count);
         if (peekedMessages.isEmpty()) {
             __peekedMessages();
-        } else  {
+        } else if (peekedMessages.size() < pendingCount) {
             try {
                 executorService.submit(() -> __peekedMessages());
             } catch (RejectedExecutionException rejectedExecutionException) {
@@ -56,18 +67,31 @@ public abstract class QueueMonitor {
         }
 
         long now = clock.millis();
-        peekedMessages.drainTo(messages, count);
-        if(messages.isEmpty()) {
-            Uninterruptibles.sleepUninterruptibly(waitTime, timeUnit);
-            peekedMessages.drainTo(messages, count - messages.size());
-        }
-
-        //Remove any expired messages...
-        boolean hasExpiredMessages = messages.stream().parallel().anyMatch(msg -> now > msg.getExpiry());
-        if(hasExpiredMessages) {
-            peekedMessages.clear();
-            pollCount.addAndGet(count);
-            return new ArrayList<>();
+        boolean waited = false;
+        for (int i = 0; i < count; i++) {
+            try {
+                // Why not use poll with timeout?
+                // poll with timeout method seem to be using spinlock that takes up more CPU
+                // The sleep method below, just does Thread.wait should be more CPU friendly
+                QueueMessage message = peekedMessages.poll();
+                if (message == null) {
+                    if (!waited) {
+                        Uninterruptibles.sleepUninterruptibly(waitTime, timeUnit);
+                        waited = true;
+                        continue;
+                    } else {
+                        return messages;
+                    }
+                }
+                if (now > message.getExpiry()) {
+                    peekedMessages.clear();
+                    pollCount.addAndGet(count);
+                    return new ArrayList<>();
+                }
+                messages.add(message);
+            } catch (Exception e) {
+                log.error(e.getMessage(), e);
+            }
         }
         return messages;
     }
@@ -93,7 +117,8 @@ public abstract class QueueMonitor {
                     log.warn("Negative poll count {}", pollCount.get());
                     pollCount.set(0);
                 }
-                //Negative number shouldn't happen, but it can be zero and in that case we don't do anything!
+                // Negative number shouldn't happen, but it can be zero and in that case we don't do
+                // anything!
                 return;
             }
 
@@ -112,23 +137,18 @@ public abstract class QueueMonitor {
                 String id = response.get(i);
                 String scoreString = response.get(i + 1);
 
-                int priority = new BigDecimal(scoreString).remainder(BigDecimal.ONE).multiply(HUNDRED).intValue();
+                int priority =
+                        new BigDecimal(scoreString)
+                                .remainder(BigDecimal.ONE)
+                                .multiply(HUNDRED)
+                                .intValue();
                 QueueMessage message = new QueueMessage(id, "", timeout, priority);
                 message.setExpiry(messageExpiry);
                 peekedMessages.add(message);
             }
             pollCount.addAndGet(-1 * (response.size() / 2));
-        } catch(Throwable t) {
+        } catch (Throwable t) {
             log.warn(t.getMessage(), t);
         }
-    }
-
-    private long getQueuedMessagesLen() {
-        long now = clock.millis();
-        if (now > nextUpdate) {
-            size = queueSize();
-            nextUpdate = now + 100; // Cache for 100 ms
-        }
-        return size;
     }
 }
